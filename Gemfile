3f30d1c94525ba2268ad2758cfca685b0dc75503
Lal Gurung <ljgpok@gmail.com>
2016-11-05 15:02:58 +0000

    create chefs resources and add authentication

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'mini_magick', '3.8.0'
 gem 'will_paginate', '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
 gem 'autoprefixer-rails'
+gem 'bcrypt', '3.1.7'
 
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,16 @@
 class RecipesController < ApplicationController
 
+    before_action :set_recipe, only: [:edit, :update, :show, :like]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update]
+
     def index
         @recipes = Recipe.paginate(page: params[:page], per_page: 5)
     
     end
     
     def show
-        @recipe = Recipe.find(params[:id])
+        
     end
     
     def new
@@ -16,9 +20,8 @@ class RecipesController < ApplicationController
     
     def create
         @recipe = Recipe.new(recipe_params)
-        @recipe.chef = Chef.find(2)
-    
-    
+        @recipe.chef = current_user
+        
         if @recipe.save
             flash[:success] = "Your recipe was created successfully!"
             redirect_to recipes_path
@@ -28,14 +31,13 @@ class RecipesController < ApplicationController
     end
     
     def edit
-       @recipe = Recipe.find(params[:id])
+       
        
     end
     
     def update
-        @recipe = Recipe.find(params[:id])
+        
         if @recipe.update(recipe_params)
-            #do something
             flash[:success] = "Your recipe was updated successfully!"
             redirect_to recipe_path(@recipe)
         else
@@ -44,8 +46,8 @@ class RecipesController < ApplicationController
     end
     
     def like
-        @recipe = Recipe.find(params[:id])
-        like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+        
+        like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
         if like.valid?
             flash[:success] = "Your selection was succesful"
             redirect_to :back
@@ -60,6 +62,17 @@ class RecipesController < ApplicationController
         
         def recipe_params
             params.require(:recipe).permit(:name, :summary, :description, :picture)
+        end
+        
+        def set_recipe
+            @recipe = Recipe.find(params[:id])
+        end
+        
+        def require_same_user
+            if current_user != @recipe.chef
+                flash[:danger] = "You can only edit your own recipes"
+                redirect_to root_path
+            end
         end
 
 end
